928d45b3fc1d7408f0fca8117e4708f43232d20f
rafelafrance <raphael.lafrance@gmail.com>
2024-11-21 15:48:58 -0500

    Add some gynoecium traits

--- angiosperm/pylib/pipelines/gynoecium.py
+++ angiosperm/pylib/pipelines/gynoecium.py
@@ -1,12 +1,22 @@
 from angiosperm.pylib.pipelines import base
 from angiosperm.pylib.rules.number_of_carpels import NumberOfCarpels
+from angiosperm.pylib.rules.number_of_whorls import NumberOfWhorls
+from angiosperm.pylib.rules.phyllotaxy import Phyllotaxy
 from angiosperm.pylib.rules.range import Range
 
 
 def build():
+    structure = "gynoecium"
+
     nlp = base.setup()
 
     Range.pipe(nlp)
     NumberOfCarpels.pipe(nlp)
+
+    NumberOfWhorls.structure = structure
+    NumberOfWhorls.pipe(nlp)
+
+    Phyllotaxy.structure = structure
+    Phyllotaxy.pipe(nlp)
 
     return base.teardown(nlp)
